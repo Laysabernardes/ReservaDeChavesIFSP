19c62e22d6fd38f830a0e762be0c5f92022c0a17
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2023-11-11 17:57:43 -0300

    updt Home e CSS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,76 +5,131 @@ function App() {
   return (
 <div>
 <Header/> {/*comando para importar o header*/}
-  <div className="containerSemNome">
+<main>
+  {/* <div className="containerSemNome">
   <h2 className="containerH2">Todas as Chaves:</h2>
   <button className="editar">Editar</button>
-  </div>
+  </div> */}
   
-  {/*CONTAINER 1*/}
-    <div className="containerChaves"> 
-      <div className="disponivel">
-        <>Laboratório 1</>
+  {/*CONTAINER DAS SALAS*/}
+    <section id="Salas">
+      <div className="tituloContainer">
+        <h2>Salas de Aula:</h2>
+        <a href="#">Ver tudo</a>
       </div>
-      <div className="disponivel">
-        <>Laboratório 2</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 3</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-      <div className="indisponivel">
-        <>Laboratório 4</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-    </div>
+      <article className="chavesContainer">
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Sala 1</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Sala 1</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Sala 1</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Sala 1</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="indisponivel"></div>
+          <p>Sala 1</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Sala 1</p>
+          <span>Reservar</span>
+        </div>
+      </article>
+    </section>
     {/*CONTAINER 2*/}
-    <div className="containerChaves"> 
-      <div className="disponivel">
-        <>Laboratório 1</>
+    <section id="Laboratorios">
+      <div className="tituloContainer">
+        <h2>Laboratórios:</h2>
+        <a href="#">Ver tudo</a>
       </div>
-      <div className="indisponivel">
-        <>Laboratório 2</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 3</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-    </div>
+      <article className="chavesContainer">
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Laboratório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="indisponivel"></div>
+          <p>Laboratório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Laboratório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Laboratório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Laboratório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Laboratório</p>
+          <span>Reservar</span>
+        </div>
+      </article>
+    </section>
     {/*CONTAINER 3*/}
-    <div className="containerChaves"> 
-      <div className="disponivel">
-        <>Laboratório 1</>
+    <section id="Diversos">
+    <div className="tituloContainer">
+        <h2>Diversos:</h2>
+        <a href="#">Ver tudo</a>
       </div>
-      <div className="disponivel">
-        <>Laboratório 2</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 3</>
-      </div>
-      <div className="indisponivel">
-        <>Laboratório 4</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-      <div className="disponivel">
-        <>Laboratório 4</>
-      </div>
-    </div>
+      <article className="chavesContainer">
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Auditório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Auditório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Auditório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Auditório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="disponivel"></div>
+          <p>Auditório</p>
+          <span>Reservar</span>
+        </div>
+        <div className="Carta">
+          <div className="indisponivel"></div>
+          <p>Auditório</p>
+          <span>Reservar</span>
+        </div>
+      </article>
+    </section>  
+</main>
 </div>
-      
   );
 }
 
