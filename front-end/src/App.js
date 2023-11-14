e11ed270f8a257c9fc3899afbccdeda2476a7ada
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2023-11-14 18:14:46 -0300

    add banner

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import Header from './pages/header.js';
+import Banner from './pages/banner.js';
 import Main from './pages/main.js'
 import Footer from './pages/footer.js'
 import './css/App.css';
@@ -7,6 +8,7 @@ function App() {
   return (
 <div>
   <Header/> {/*comando para importar o header*/}
+  <Banner/>{/*comando para importar o banner*/}
   <Main/> {/*comando para importar o header*/}
   <Footer/> {/*comando para importar o Footer*/}
 </div>
---
+++ front-end/src/pages/banner.js
@@ -0,0 +1,19 @@
+// Arquivo: Banner.js
+import React from 'react';
+
+import '../css/banner.css';  // Importa o estilo CSS para o componente Header
+
+function Banner() {
+  return (
+    // Elemento React representando a estrutura do HTML
+    <section className='banner'>
+        <div className='banner__conteudo container'>
+            <h2 className='banner__titulo'>Campus Cubatão</h2>
+            <p className='banner__texto'>Sistema de reserva de chaves.</p>
+            <a href="#" className='banner__botao'>Ver chaves</a>
+        </div>
+    </section>
+  );
+}
+
+export default Banner;  // Exporta o componente Banner para ser usado em outros lugares do projeto
