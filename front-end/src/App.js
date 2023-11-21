6a8b40b2323f8fc20d5d0b3f2247b868798558c6
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-11-21 15:19:08 -0300

    Adição da biblioteca axios(front)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,22 @@
-//page App.js
-
-
+// Importa o componente de página principal (Main) e o estilo CSS do aplicativo
 import Main from './pages/main.js'
 import './css/App.css'
 import React, { Component } from "react";
 import Login from "./pages/login.js";
 
+import Api from "./api.js"
+const api = new Api();
 
+// Classe do componente principal do aplicativo
 class App extends Component {
+  // Estado inicial do componente, determina se o componente de login deve ser exibido
   state = {
     showLogin: true,
   };
 
+  // Método do ciclo de vida do componente, é chamado após o componente ser montado na tela
   componentDidMount() {
-    // Define o estado `showLogin` para `false` após 5 segundos
+    // Define o estado `showLogin` para `false` após 5 segundos (simulando um processo de autenticação)
     setTimeout(() => {
       this.setState({
         showLogin: false,
@@ -21,13 +24,16 @@ class App extends Component {
     }, 5000);
   }
 
+  // Método de renderização do componente
   render() {
     return (
       <div>
+        {/* Renderiza o componente de login se `showLogin` for true, caso contrário, renderiza a página principal (Main) */}
         {this.state.showLogin ? <Login /> : <Main />}
       </div>
     );
   }
 }
 
+// Exporta o componente principal do aplicativo para ser utilizado em outros lugares
 export default App;
---
+++ front-end/src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class Api {
+
+  // Configuração padrão do Axios http://localhost:3001 
+  api = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
+  
+  // Exemplo de função para fazer uma solicitação de registro de usuário
+  registerUser = (userData) => {
+    return this.api.post('/users/', userData);
+  };
+  
+  // Exemplo de função para fazer uma solicitação de login de usuário
+  loginUser = (userData) => {
+    return this.api.post('/auth/', userData);
+  };
+}
+
+export default Api;
