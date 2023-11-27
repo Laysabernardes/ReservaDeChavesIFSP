d9a4d1b5b04174279b315bd7cd00680cab87dbd0
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-11-27 14:19:48 -0300

    Logica de redirecionamento login -> main ok

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -13,7 +13,6 @@ import '../css/login.css';
 // Importa componentes de cabeçalho e rodapé
 import Header from './header';
 import Footer from './footer';
-import Routes from '../Routes.js';
 
 
 // Define o componente funcional LoginForm
@@ -23,6 +22,7 @@ function LoginForm() {
   const [senha, setSenha] = useState('');
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginError, setLoginError] = useState(false);
 
    // Obtém a função navigate
   const navigate = useNavigate();
@@ -37,6 +37,7 @@ function LoginForm() {
       console.log('Resposta da API:', response.data);
 
       const user = response.data;
+      const userName = response.data.user[0].nm_solicitante;
 
       // Atualiza os estados com os dados do usuário e o estado de login
       setIsLoggedIn(true);
@@ -44,15 +45,18 @@ function LoginForm() {
       // Imprime informações para depuração
       console.log('Login:', login);
       console.log('Senha:', senha);
-      console.log('Nome do usuário:', user.nm_solicitante);
+      console.log('Nome do usuário:', userName);
 
-      if (user) {
+      if (userName) {
+        setIsLoggedIn(true);
+        setLoginError(false);
         // Usa navigate para redirecionar o usuário para a página principal após o login bem-sucedido
         navigate('/main');
       }
     } catch (error) {
       // Trata erros de login, define o estado de login como falso e exibe uma mensagem de alerta
       setIsLoggedIn(false);
+      setLoginError(true);
       alert("Login ou senha incorretos");
       console.error('Erro ao realizar login:', error);
     }
@@ -67,70 +71,68 @@ function LoginForm() {
   // Retorna a estrutura do componente JSX
   return (
     <div className="App">
-      {/* Renderiza o componente de cabeçalho */}
-      <Header />
-
-      {/* Renderiza a seção de formulário */}
-      <div className="formulario">
-        <div className="formulario-login container">
-          <h3 className="formulario-login__titulo">Iniciar Sessão</h3>
-
-          {/* Renderiza o formulário com a função handleLogin sendo chamada no evento onSubmit */}
-          <form className="formulario-login_form" onSubmit={handleLogin}>
-
-            {/* Renderiza o campo de login */}
-            <div className="formulario-login__input-container">
-              <input
-                name="login"
-                id="login"
-                className="input inputs"
-                type="text"
-                placeholder="Login"
-                required
-                value={login}
-                onChange={(e) => setLogin(e.target.value)}
-              />
-              <label className="input-label" htmlFor="login">
-                Escreva seu login
-              </label>
-              <span className="input-message-error">Este campo não é válido</span>
-            </div>
-
-            {/* Renderiza o campo de senha */}
-            <div className="formulario-login__input-container">
-              <input
-                name="senha"
-                id="senha"
-                className="input inputs"
-                type="password"
-                placeholder="Senha"
-                required
-                value={senha}
-                onChange={(e) => setSenha(e.target.value)}
-              />
-              <label className="input-label" htmlFor="senha">
-                Escreva sua senha
-              </label>
-              <span className="input-message-error">Este campo não é válido</span>
-            </div>
-
-            {/* Renderiza o botão de enviar */}
-            <input
-              className="boton-formulario-login"
-              type="submit"
-              name="enviar"
-              id="enviar-login"
-              value="Entrar"
-            />
-          </form>
-        </div>
+    {isLoggedIn ? (
+      <div className="main-content">
+        {/* Conteúdo da página main pode ser renderizado aqui */}
       </div>
-
-      {/* Renderiza o componente de rodapé */}
-      <Footer />
-    </div>
-  );
+    ) : (
+      <div>
+        <Header />
+        <div className="formulario">
+          <div className="formulario-login container">
+            <h3 className="formulario-login__titulo">Iniciar Sessão</h3>
+            <form className="formulario-login_form" onSubmit={handleLogin}>
+              <div className="formulario-login__input-container">
+                <input
+                  name="login"
+                  id="login"
+                  className="input inputs"
+                  type="text"
+                  placeholder="Login"
+                  required
+                  value={login}
+                  onChange={(e) => setLogin(e.target.value)}
+                />
+                <label className="input-label" htmlFor="login">
+                  Escreva seu login
+                </label>
+                <span className="input-message-error">Este campo não é válido</span>
+              </div>
+              <div className="formulario-login__input-container">
+                <input
+                  name="senha"
+                  id="senha"
+                  className="input inputs"
+                  type="password"
+                  placeholder="Senha"
+                  required
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                />
+                <label className="input-label" htmlFor="senha">
+                  Escreva sua senha
+                </label>
+                <span className="input-message-error">Este campo não é válido</span>
+              </div>
+              <input
+                className="boton-formulario-login"
+                type="submit"
+                name="enviar"
+                id="enviar-login"
+                value="Entrar"
+              />
+            </form>
+            {loginError && <p className="error-message">Login ou senha incorretos.</p>}
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )}
+  </div>
+);
 }
+
+
 
 // Exporta o componente LoginForm para ser utilizado em outras partes do código
 export default LoginForm;
