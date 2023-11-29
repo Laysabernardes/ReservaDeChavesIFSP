0b0364c912ba7ace704f69f2308137296dd74cb9
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2023-11-29 17:56:32 -0300

    .

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -4,11 +4,9 @@ const {Router} = require("express");
 const localRouter = require("./chaves.routes");
 const userRouter = require("./user.routes");
 const reservaRouter = require("./reserva.routes");
-<<<<<<< HEAD
 const permissaoRouter = require("./permissao.routes");
-=======
 const SolicitacaoRouter = require("./solicitacao.route")
->>>>>>> 7ce3f26db5acc60e5fb4a0281b889c12398c4b0c
+
 
 const router = Router();
 router.use("/chaves", localRouter);
