714a2dbd8158abccebcfabe1105ffbbcf4196cca
Eduardo Miranda <eduardomssousa@hotmail.com>
2023-12-10 02:21:00 -0300

    Rota privada adm (atualmente em teste)

---
+++ front-end/src/AccessAdm.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+
+export function AccessAdm({ children }) {  
+  // Cria um efeito para buscar o cargo do usuário do backend
+  useEffect(() => {
+    // Define uma função assíncrona para fazer a requisição
+    async function findCargo() {
+      try {
+        // Faz a requisição usando axios
+        const user = JSON.parse(localStorage.getItem('userData'));
+        if ( user === '701001') {
+          return children;
+        } else {
+          // Caso contrário, navega para a página inicial
+          return <Navigate to="/main" />;
+        }
+      } catch (error) {
+        // Se ocorrer algum erro, trata-o de acordo
+        console.error(error);
+      }
+    }
+
+    // Invoca a função assíncrona
+    findCargo();
+  
+  }, []);
+      
+  return (
+    <div>
+      {children}
+      </div>
+    )
+  
+}
