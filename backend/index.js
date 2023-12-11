343f7e5c9898e1cbf9873f256969b5b360185a2b
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2023-12-11 19:05:32 -0300

    Update(URL mudou para o server)(Frontend e Backend)

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,35 @@
 const express = require('express');
-    // Importa o express, que é uma biblioteca para criar aplicativos web em Node.js.
-const connection =  require("./src/database/db.js");
-    // Importa o connection de db.js para conectar ao MongoDB.
+// Importa o express, que é uma biblioteca para criar aplicativos web em Node.js.
+const connection = require("./src/database/db.js");
+// Importa o connection de db.js para conectar ao MongoDB.
 const routes = require("./src/routes/routes.js");
-    // Importa o authRoute do arquivo auth.route.js para definir rotas de autenticação.
-const dotenv = require ("dotenv");
-    // Importa o'dotenv' para carregar variáveis de ambiente a partir de um arquivo chamado '.env'.
+// Importa o authRoute do arquivo auth.route.js para definir rotas de autenticação.
+const dotenv = require("dotenv");
+// Importa o'dotenv' para carregar variáveis de ambiente a partir de um arquivo chamado '.env'.
 
 
 var cors = require('cors');
 
 dotenv.config();
-    // Configura as variáveis de ambiente a partir do arquivo '.env'.
+// Configura as variáveis de ambiente a partir do arquivo '.env'.
 
 const app = express();
-    // Cria uma instância do aplicativo Express.
+// Cria uma instância do aplicativo Express.
 
-const port = process.env.PORT || 3001;
-    // Define uma porta para o servidor, usando a variável de ambiente 'PORT' se estiver definida, caso contrário, usa a porta 3000.
+const port = process.env.PORT || 'http://10.111.50.229/ReservaDeChavesIFSP/backend';
+
+// Define uma porta para o servidor, usando a variável de ambiente 'PORT' se estiver definida, caso contrário, usa a porta 3000.
 
 connection.connect();
-    // Conecta ao banco de dados.
+// Conecta ao banco de dados.
 app.use(cors());
 
 app.use(express.json());
-    // Define que a aplicação Express irá lidar com solicitações e respostas em formato JSON.
-    
-    app.use(express.urlencoded({ extended: true }));
+// Define que a aplicação Express irá lidar com solicitações e respostas em formato JSON.
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-    // Associa as rotas definidas no arquivo 'auth.route.js' à aplicação Express, prefixando-as com "/auth".
+// Associa as rotas definidas no arquivo 'auth.route.js' à aplicação Express, prefixando-as com "/auth".
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
-    // Inicia o servidor Express e escuta na porta definida, exibindo uma mensagem quando o servidor está rodando.
+// Inicia o servidor Express e escuta na porta definida, exibindo uma mensagem quando o servidor está rodando.
