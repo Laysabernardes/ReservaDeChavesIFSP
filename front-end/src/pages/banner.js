bd1d05ed0203ae8de95962039ed34cc4d5764026
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2023-12-10 22:46:07 -0300

    Add(Botões exclusivos dependendo do cargo)(Frontend)

--- front-end/src/pages/banner.js
+++ front-end/src/pages/banner.js
@@ -6,8 +6,10 @@ import { useSolicitacoes } from '../SolicitacoesContext';
 import '../css/banner.css';
 
 function Banner() {
+  const [mostraAdm, setMostraAdm] = useState(true);
   const [mostraProf, setMostraProf] = useState(true);
   const [mostraAluno, setmostraAluno] = useState(true);
+  const [mostraDefault, setmostraDefault] = useState(true);
   const navigate = useNavigate();
   const userData = JSON.parse(localStorage.getItem('userData'));
   const cargo = userData.cd_cargo;
@@ -15,15 +17,29 @@ function Banner() {
   useEffect(() => {
     switch (cargo) {
       case 'A0001':
+        setMostraAdm(false)
         setMostraProf(false);
         setmostraAluno(true);
+        setmostraDefault(false);
         break;
       case '701001':
+        setMostraAdm(true)
         setMostraProf(false);
+        setmostraDefault(false);
         setmostraAluno(false);
-      default:
+        break;
+      case '707001':
+        setMostraAdm(false)
         setMostraProf(true);
         setmostraAluno(false);
+        setmostraDefault(false);
+        break;
+      default:
+        setMostraAdm(false)
+        setMostraProf(false);
+        setmostraAluno(false);
+        setmostraDefault(true);
+        break;
     }
   }, [cargo]);
 
@@ -34,7 +50,7 @@ function Banner() {
         <p className='banner__texto'>Sistema de reserva de chaves.</p>
 
         {/* Renderiza o botão somente se mostraProf for true */}
-        {mostraProf && (
+        {mostraProf && (<>
           <button
             className="banner__botao"
             onClick={() => {
@@ -45,20 +61,6 @@ function Banner() {
           >
             Pedidos de acesso de Alunos
           </button>
-        )}
-        {/* Renderiza o botão somente se mostraProf for true */}
-        {mostraAluno && (
-          <button
-            className="banner__botao"
-            onClick={() => {
-              setTimeout(() => {
-                navigate('/pedidosEstudante');
-              }, 100);
-            }}
-          >
-            Verificar suas permissões
-          </button>
-        )}
           <button
             className="banner__botao reservas__botao"
             onClick={() => {
@@ -69,6 +71,59 @@ function Banner() {
           >
             Verificar suas reservas
           </button>
+        </>
+        )}
+        {/* Renderiza o botão somente se mostraAluno for true */}
+        {mostraAluno && (<>
+          <button
+            className="banner__botao"
+            onClick={() => {
+              setTimeout(() => {
+                navigate('/pedidosEstudante');
+              }, 100);
+            }}
+            >
+            Verificar suas permissões
+          </button>
+          <button
+            className="banner__botao reservas__botao"
+            onClick={() => {
+              setTimeout(() => {
+                navigate('/acompanharPedidos');
+              }, 100);
+            }}
+          >
+            Verificar suas reservas
+          </button>
+        </>
+          
+        )}
+        {/* Renderiza o botão somente se mostraAdm for true */}
+        {mostraAdm && (
+          <button
+            className="banner__botao"
+            onClick={() => {
+              setTimeout(() => {
+                navigate('/pedidosAdm');
+              }, 100);
+            }}
+          >
+            Verificar pedidos de reserva
+          </button>
+        )}
+        {/* Renderiza o botão somente se mostraDefault for true */}
+        {mostraDefault && (
+          <button
+            className="banner__botao"
+            onClick={() => {
+              setTimeout(() => {
+                navigate('/acompanharPedidos');
+              }, 100);
+            }}
+          >
+            Verificar pedidos de reserva
+          </button>
+        )}
       </div>
     </section>
   );
