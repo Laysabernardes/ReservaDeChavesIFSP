19e9d61bb0e4a31c4318f9a3ada2b485e0127cd9
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-12-12 06:00:22 -0300

    Update (axios)(fornt)

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,25 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://backend-reserva-ifsp.onrender.com/', // Substitua pela URL do seu backend
+    baseURL: 'https://backend-reserva-ifsp.onrender.com/', 
+    // ou http://localhost:3001/ 
   });
 
 export default instance;
-
-// class Api {
-
-//   // Configuração padrão do Axios http://localhost:3001 
-//   api = axios.create({
-//     baseURL: 'http://localhost:3001',
-//   });
-  
-//   // Exemplo de função para fazer uma solicitação de registro de usuário
-//   registerUser = (userData) => {
-//     return this.api.post('/users/', userData);
-//   };
-  
-//   // Exemplo de função para fazer uma solicitação de login de usuário
-//   loginUser = (userData) => {
-//     return this.api.post('/auth/', userData);
-//   };
-// }
